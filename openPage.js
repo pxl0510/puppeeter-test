8b55878e290d4b5778db09683cb5701a8b83f7d5
gyp <guoyaping.gyp@alibaba-inc.com>
2024-02-24 16:01:24 +0800

    login

--- openPage.js
+++ openPage.js
@@ -23,7 +23,27 @@ const openPage = async (options) => {
     await page.setCookie(...options.cookies);
     await page.cookies(options.pageUrl);
   }
-  await page.goto(options.pageUrl);
+
+  await page.goto(options.pageUrl, { waitUntil: "domcontentloaded" });
+
+  // 输入用户名和密码
+  await page.type("#fm-login-id", "pxl_0510@163.com", {
+    delay: 100,
+  }); //#username 是输入用户名的输入框的select选择器，与css选择方式一致，下方密码输入一致
+  await page.type("#fm-login-password", "pxl_0510", {
+    delay: 100, //键盘按下和释放间隔的时间，单位为毫秒
+  });
+
+  // 提交表单
+  await Promise.all([
+    page.click("#fm-login-submit"), //#btnSubmit 登录按钮的id
+    page.waitForNavigation(), //如果点击事件发生跳转，会有一个独立的promise对象需要等待（等待页面跳转）
+  ]);
+
+  setTimeout(() => {
+    page.click(".next-dialog-close");
+  }, 3000);
+   
 
   return {
     page,
